c0ca10a80a8a6bac3abe1584bd14c1fc0f18b9b4
Viacheslav Gorchakov <gorchakovslav@gmail.com>
2023-09-28 17:52:35 +0200

    small preprocess improvement

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -12,19 +12,18 @@ class DataPreprocess():
 
     def preprocess_data(self):
         flights_data = pd.read_csv('data/azul_airline_flights.csv')
-        flights_data['Departure_Date_Day'] = pd.to_datetime(flights_data['Scheduled.Departure']).dt.date
-        flights_data['Departure_Date'] = pd.to_datetime(flights_data['Scheduled.Departure']).dt.date
+        flights_data['Scheduled.Departure'] = pd.to_datetime(flights_data['Scheduled.Departure'])
+        flights_data['Scheduled.Arrival'] = pd.to_datetime(flights_data['Scheduled.Arrival'])
+        flights_data['Departure_Date'] = flights_data['Scheduled.Departure'].dt.date
+
         # take one day with maximum number of flights
-        day_max_number_flights = flights_data.groupby('Departure_Date_Day').size().idxmax()
+        day_max_number_flights = flights_data.groupby('Departure_Date').size().idxmax()
         one_day_flights_df = flights_data[flights_data['Departure_Date'] == day_max_number_flights]
-        one_day_flights_df = one_day_flights_df[
-            ['Airline', 'Scheduled.Departure', 'Scheduled.Arrival', 'Airport.From', 'Airport.To',
-             'Longitude.To', 'Latitude.To', 'Longitude.From', 'Latitude.From']]
+
+        # preprocess dataframe of one day flights
+        day_max_number_flights = pd.to_datetime(day_max_number_flights)
         one_day_flights_df = one_day_flights_df.loc[
             one_day_flights_df['Scheduled.Departure'].notna() & one_day_flights_df['Scheduled.Arrival'].notna()]
-        one_day_flights_df['Scheduled.Departure'] = pd.to_datetime(one_day_flights_df['Scheduled.Departure'])
-        one_day_flights_df['Scheduled.Arrival'] = pd.to_datetime(one_day_flights_df['Scheduled.Arrival'])
-        day_max_number_flights = pd.to_datetime(day_max_number_flights)
         one_day_flights_df['departure_time'] = (
                 one_day_flights_df['Scheduled.Departure'] - day_max_number_flights).dt.total_seconds().astype(int)
         one_day_flights_df['arrival_time'] = (
@@ -32,6 +31,7 @@ class DataPreprocess():
         one_day_flights_df = one_day_flights_df[['Airport.From', 'Airport.To', 'departure_time', 'arrival_time',
                                                  'Longitude.To', 'Latitude.To', 'Longitude.From', 'Latitude.From']]
         one_day_flights_df.rename(columns=ONE_DAY_FLIGHTS_COLUMNS, inplace=True)
+        # create unique flight index
         one_day_flights_df['flight_index'] = \
             one_day_flights_df[['airport_from', 'airport_to', 'departure_time']].apply(
                 lambda row: '_'.join(row.values.astype(str)), axis=1)
--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import json
 
 def run_pipeline():
     print('Start Data Preprocessing')
-    # dp = DataPreprocess()
-    one_day_flights_df = pd.read_csv('data/day_flights.csv')
-    flight_info_dict = DataPreprocess.construct_graph_input(one_day_flights_df)
-
+    dp = DataPreprocess()
+    #one_day_flights_df = pd.read_csv('data/day_flights.csv')
+    #flight_info_dict = DataPreprocess.construct_graph_input(one_day_flights_df)
     print('Start Building Graph')
-    gr = BuildGraph(flight_info_dict)
+    gr = BuildGraph(dp.flight_info_dict)
     print('Start Model Solving')
     m = ModelBuild(gr.flight_list, gr.flight_pairs_dict, gr.incoming_nodes, gr.outgoing_nodes)
     # store model output
     with open("model_output.json", "w") as file:
         json.dump(m.solved_model.model_variable_values, file)
 
-    VisualizeSchedule(m.solved_model, flight_info_dict)
+    VisualizeSchedule(m.solved_model, dp.flight_info_dict)
 
 
 if __name__ == "__main__":
